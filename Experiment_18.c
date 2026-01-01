9b5c4ccd5d228be04dac0737bd8ca004ddb4870e
Sourav choudhary <sc529677@gmail.com>
2026-01-01 15:48:52 +0530

    Create Experiment_18.c

---
+++ Experiment_18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int rows = 3;
+    int cols = 3;
+    int sum = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sum += arr[i][j];
+        }
+    }
+    printf("The sum of all elements in the 2D array is: %d\n", sum);
+    return 0;
+}
