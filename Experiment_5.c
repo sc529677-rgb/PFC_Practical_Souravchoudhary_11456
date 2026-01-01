4b2e16921013d859bb83f6f43eb5ddb1b275744c
Sourav choudhary <sc529677@gmail.com>
2026-01-01 15:44:03 +0530

    Create Experiment_5.c

---
+++ Experiment_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i = 42;                 
+    float f = 3.14159;           
+    double d = 123.456789;       
+    char c = 'A';
+    printf("Integer:%d\n",i);
+    printf("Float:%f\n",f);
+    printf("Double:%f\n",d);
+    printf("Character:%c\n",c);
+      
+    return 0;
+}
