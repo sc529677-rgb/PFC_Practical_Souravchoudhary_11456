f4970069754f8e0136aa4310af59bfd21d01cd9d
Sourav choudhary <sc529677@gmail.com>
2026-01-01 15:45:18 +0530

    Create Experiment_8.c

---
+++ Experiment_8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("Number is even");
+    }else{
+        printf("Number is odd");
+    }
+    return 0;
+}
